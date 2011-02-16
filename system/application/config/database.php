33409120e98e13d36e6378c7e6021a38e6607413
adriacidre <adria.cidre@gmail.com>
2011-02-16 01:16:03 -0800

    Edited system/application/config/database.php via GitHub

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -39,8 +39,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "c13nc13s";
-$db['default']['database'] = "vetathome";
+$db['default']['password'] = "your_password";
+$db['default']['database'] = "your_database";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
